c527931aa1917b32628e7bc1deb38f5006cfebe9
Ryo-cool <coolboxing576@gmail.com>
2022-02-23 22:52:22 +0900

    TailwindCss

---
+++ front/app/pages/index.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import { GetStaticProps } from "next";
+
+type Post = {
+  id: number;
+  title: string;
+}
+
+type Props = {
+  posts: Post[];
+}
+
+const Home: FC<Props> = (props) => {
+  return (
+    <div>
+      <h2>POSTの一覧</h2>
+      <div className="text-red-400">おはよう</div>
+      <h1 className='text-blue-500'>ありがとう</h1>
+      <table>
+	{props.posts.map((post) =>
+	  <tr>
+	    <td>{post.id}.</td>
+	    <td>{post.title}</td>
+	  </tr>
+        )}
+      </table>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const response = await fetch("http://api:3000/posts", {method: "GET"});
+  const json = await response.json();
+
+  return {
+    props: {
+      posts: json
+    },
+  };
+}
+
+export default Home;
